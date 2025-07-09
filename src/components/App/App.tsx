dd3102a014fd5eb275781be0c2dd448e08805eb5
Andrew Kruhluk <kruglikandrej9@gmail.com>
2025-07-09 23:29:21 +0300

    last axios

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,7 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 import fetchMovies from "../../services/movieService";
-import { Axios } from "axios";
+
 
 export default function App(){
 
